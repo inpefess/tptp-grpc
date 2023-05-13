c2fc3c7158d08e5cd527ad4dece7d37191e6f60c
Boris Shminke <boris@shminke.ml>
2023-05-13 12:35:00 +0200

    make client return the state

--- tptp_grpc_client/src/main/java/com/github/inpefess/tptp_grpc/tptp_grpc_client/TPTPCNFgRPCClient.java
+++ tptp_grpc_client/src/main/java/com/github/inpefess/tptp_grpc/tptp_grpc_client/TPTPCNFgRPCClient.java
@@ -18,7 +18,6 @@
 package com.github.inpefess.tptp_grpc.tptp_grpc_client;
 
 import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import com.github.inpefess.tptp_grpc.tptp2proto.TPTPCNFParserServer;
 import com.github.inpefess.tptp_grpc.tptp_proto.SaturationProofState;
@@ -28,7 +27,6 @@ import io.grpc.Channel;
 import io.grpc.Grpc;
 import io.grpc.InsecureChannelCredentials;
 import io.grpc.ManagedChannel;
-import io.grpc.StatusRuntimeException;
 
 /**
  * A simple client that requests to parse a TPTP CNF string from the {@link TPTPCNFParserServer}.
@@ -48,17 +46,11 @@ public class TPTPCNFgRPCClient {
   }
 
   /** Send a string to parse to server. */
-  public void parseCNF(String name) {
-    logger.info("Will try to parse " + name + " ...");
+  public SaturationProofState parseCNF(String name) {
     StringMessage request = StringMessage.newBuilder().setStringMessage(name).build();
     SaturationProofState response;
-    try {
-      response = blockingStub.parseCNF(request);
-    } catch (StatusRuntimeException e) {
-      logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
-      return;
-    }
-    logger.info("Parsing result: " + response.toString());
+    response = blockingStub.parseCNF(request);
+    return response;
   }
 
   /**
@@ -94,8 +86,9 @@ public class TPTPCNFgRPCClient {
     ManagedChannel channel =
         Grpc.newChannelBuilder(target, InsecureChannelCredentials.create()).build();
     try {
+      logger.info("Parsing string: " + cnfString);
       TPTPCNFgRPCClient client = new TPTPCNFgRPCClient(channel);
-      client.parseCNF(cnfString);
+      logger.info("Parsing result: " + client.parseCNF(cnfString).toString());
     } finally {
       // ManagedChannels use resources like threads and TCP connections. To prevent leaking these
       // resources the channel should be shut down when it will no longer be used. If it may be used
